6b60cac581d1efada02cc8a50f32222a7b149f57
Manh Nguyen <manh.nguyen@thirdsight.net>
2025-02-23 13:09:06 +0800

    Remove blank line

--- mvcwcms-4.1.1/sql-scripts/2.sql
+++ mvcwcms-4.1.1/sql-scripts/2.sql
@@ -6,7 +6,6 @@ SELECT TOP (1000) [UserName]
       ,[GroupId]
   FROM [MVCwCMS].[dbo].[tb_admin_users]
 
-
   select * from tb_cms_subscriptions
 
   update tb_admin_users set Email = N'manhng83@gmail.com' where UserName = 'Administrator'   
